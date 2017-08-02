d4dba026f5419a72871f74052790cb829f0349e5
marek matecki <ahiel@o2.pl>
2017-08-02 19:57:10 +0200

    Add study drill exercise_3.rb

--- exercise_3.rb
+++ exercise_3.rb
@@ -1,23 +1,28 @@
+# Program writes string to the output shell.
 puts "I will now count my chickens:"
-
-puts "Hens #{25 + 30 / 6}"
-puts "Roosters #{100 - 25 * 3 % 4}"
-
+# Calculate number of hens.
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# Calculate number of roosters.
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+# Print string to shell.
 puts "Now I will count the eggs:"
-
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 +6
-
-puts "Is it true that 3 + 2 < 5 - 7?"
-
-puts 3 + 2 < 5 -7
-
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7? #{5 - 7}"
-
+# Perform calculations.
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 +6.0
+# Print string to shell.
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+# Print calculations to shell.
+puts 3.0 + 2.0 < 5.0 -7.0
+# Print string with calculations.
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+# Print string with calculations.
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+# Print string to shell.
 puts "Oh, that's why it's false."
-
+# Print string to shell.
 puts "How about some more."
-
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 >= -2}"
-puts "Is it less or equal? #{5 <= -2}"
+# Print string with interpolation.
+puts "Is it greater? #{5.0 > -2.0}"
+# Print string with interpolation.
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# Print string with interpolation.
+puts "Is it less or equal? #{5.0 <= -2.0}"
