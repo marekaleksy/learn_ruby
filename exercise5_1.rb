723f139448f2ed2842c023516c88d26d302af163
marek matecki <ahiel@o2.pl>
2017-09-09 13:15:47 +0200

    add exercise_5.rb

---
+++ exercise5_1.rb
@@ -0,0 +1,30 @@
+my_name = 'Marek Matecki'
+my_age = 26 # and do not know programming yet
+my_height = 188 # centimetres
+my_weight = 103 # kilograms
+my_eyes = 'Brown'
+my_hair = 'Bald'
+my_nationality = 'Polish'
+my_hobby = 'learning programming obviously!'
+my_studies = 'philosphy'
+my_free_time_1 = 'redhead darling'
+my_free_time_2 = 'powerlifting, boxing, muay thai'
+my_free_time_3 = 'reading philosophical and theological books'
+
+
+
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_nationality}."
+puts "He's #{my_height} centimetres tall."
+puts "He's #{my_weight} kilograms heavy."
+puts "2 years ago he weighed 125 kilos."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair. Is there color like that?"
+puts "His hobby is #{my_hobby}"
+puts "He is student also. He studies #{my_studies}."
+puts "What is filling up his free time?"
+puts "Oh, primarily his #{my_free_time_1}!"
+puts "Farther #{my_free_time_3}, #{my_free_time_2}."
+
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
---
+++ vodka.rb
@@ -0,0 +1,34 @@
+puts "Let's check what is my IQ score."
+
+puts "Some time ago I had 67.871"
+
+puts "But since that day I was drinking so much vodka"
+
+puts "So let's count:"
+
+puts "Every litre of vodka is killing 9.73818361 grey matter."
+
+puts "Every grey matter is 4.997 IQ entity."
+
+puts "I drank since 23 years."
+
+puts "Every day I was drinking about 1.18946318 litre of vodka."
+
+puts "So, let's calculate:"
+
+puts "What is 1.18946318 * 9.73818361? #{1.18946318 * 9.73818361}"
+
+puts "So vodka killed my 11.583210844174479 grey matter."
+puts "And now, let's multiply:"
+puts "What is 11.583210844174479 * 4.997 ? #{11.583210844174479 * 4.997}"
+
+puts "It means, that I lost 57.88130458833987 of my IQ."
+
+puts "Therefore, what is my IQ score at present??. Just a moment."
+
+puts "What is 67.871 - 57.88130458833987?"
+
+puts "Well, ruby is saying, that it is:"
+puts 67.871 - 57.88130458833987
+
+puts "Oh, that's very very bad. I must stop drinking!"
