3fe62cbb5f87f97ca7e1d1d8bd67ba3b5eba29be
marek matecki <ahiel@o2.pl>
2017-09-11 12:35:53 +0200

    add ice_cream.rb

---
+++ ice_cream.rb
@@ -0,0 +1,39 @@
+puts "I scream."
+puts "You scream."
+puts "We all scream."
+puts "For the ice cream!"
+puts "." * 10
+puts "<.>" * 16
+puts ">.<" * 16
+
+end1 = "D"
+end2 = "o"
+end3 = "y"
+end4 = "o"
+end5 = "u"
+end6 = "l"
+end7 = "i"
+end8 = "k"
+end9 = "e"
+end10 = "i"
+end11 = "c"
+end12 = "e"
+end13 = "c"
+end14 = "r"
+end15 = "e"
+end16 = "a"
+end17 = "m"
+
+
+
+puts end1 + end2
+
+puts end3 + end4 + end5
+
+puts end6 + end7 + end8 + end9
+
+puts end10 + end11 + end12
+
+puts end13 + end14 + end15 + end16 + end17
+
+puts "?" * 123
