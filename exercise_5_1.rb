1df009faccb3c59572147febe321a46b719d555c
marek matecki <ahiel@o2.pl>
2017-09-11 11:15:50 +0200

    Add exercise_5_1.rb

---
+++ exercise_5_1.rb
@@ -0,0 +1,34 @@
+name = 'Marek Matecki'
+age = 26.78 # and do not know programming yet
+height = 188.33 # centimetres
+weight = 103.96 # kilograms
+eyes = 'Brown'
+hair = 'Bald'
+nationality = 'Polish'
+hobby = 'learning programming obviously!'
+studies = 'philosphy'
+free_time_1 = 'redhead darling'
+free_time_2 = 'powerlifting, boxing, muay thai'
+free_time_3 = 'reading philosophical and theological books'
+
+
+
+puts "I will tell you something about me."
+puts "My name is #{name}."
+puts "I am #{nationality}."
+puts "I am #{height} centimetres tall."
+puts "I am #{weight} kilograms heavy."
+puts "2 years ago I weighed 125 kilos."
+puts "I've got #{eyes} eyes and #{hair} hair. Is there color like that?"
+puts "My hobby is #{hobby}"
+puts "I'm student also. I study #{studies}."
+puts "What is filling up my free time?"
+puts "Oh, primarily my #{free_time_1}!"
+puts "Farther #{free_time_3}, #{free_time_2}."
+
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+puts "If I subtract #{height} and #{age} and multiply with #{weight} I get #{height - age * weight}."
+puts "If I multiply #{age} and #{height} and #{weight} I get #{age * height * weight}."
+puts "If I divide #{weight} with #{age} with #{height} I get #{weight / age / height}."
+puts "I can do a lot of calculations, and my friend Ruby would help me. But what for?"
