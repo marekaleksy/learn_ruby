b029f171b759394a0d85e04b52d5d8f93568c967
marek matecki <ahiel@o2.pl>
2017-09-20 20:07:54 +0200

    add exercise_11.rb

---
+++ exercise_11.rb
@@ -0,0 +1,8 @@
+print "How old are you?"
+age = gets.chomp.to_i
+print "How tall are you?"
+height = gets.chomp.to_i # centimetres
+print "How much do you weight?" # kilograms
+weight = gets.chomp.to_i
+
+puts "So you're #{age} old, #{height} tall and #{weight} heavy."
