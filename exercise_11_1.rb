4b1fd3faaede1c4b15996120965f47c09d449fe0
marek matecki <ahiel@o2.pl>
2017-09-22 19:06:47 +0200

    add exercise_11_1.rb

---
+++ exercise_11_1.rb
@@ -0,0 +1,31 @@
+puts "Hi, for help, I need some informations about you."
+
+print "So, firstly, what's your name?"
+name = gets.chomp
+
+print "Where are you from?"
+homeland = gets.chomp
+
+print "Where do you live?"
+live = gets.chomp
+
+print "What's your profession?"
+profession = gets.chomp
+
+print "How tall are you?"
+height = gets.chomp
+
+print "How much do you weight?"
+weight = gets.chomp
+
+print "Which football team  do you cheer?"
+football_team = gets.chomp
+
+print "Who is your favourite penman?"
+penman = gets.chomp
+
+print "What is your favourite colour?"
+colour = gets.chomp
+
+puts "So you are #{name}, you come from #{homeland}, you live in #{live}, you earn for being #{profession}, your measure is #{height}, you're #{weight} heavy, you're cheering on #{football_team}, and your favourite penman and colour is #{penman} and #{colour}."
+puts "I can help you know, cause I have enough informations. Thank you."
