3c16558a5da9aa12a8a540353bebc6ef1bca30a1
marek matecki <ahiel@o2.pl>
2017-09-23 14:55:38 +0200

    add exercise_12_1.rb

---
+++ exercise_12_1.rb
@@ -0,0 +1,8 @@
+print "Ok, you owe me 12.47417541792451 % of your income."
+print "Give me an amount of your present income: "
+
+income = gets.chomp.to_f
+
+debt = income * 0.1247417541792451
+puts "So your present debt is #{debt}."
+puts "GIVE ME ME MONEY! "
