79833404418a04a2b194410e081c8485b312374b
marek matecki <ahiel@o2.pl>
2017-09-23 17:18:06 +0200

    add exercise_13_1.rb

---
+++ exercise_13_1.rb
@@ -0,0 +1,7 @@
+first, second, third, fourth, fifth = ARGV
+
+puts "Your first argument is: #{first}"
+puts "Your second argument is: #{second}"
+puts "Your third argument is: #{third}"
+puts "Your fourth argument is: #{fourth}"
+puts "Your fifth argument is: #{fifth}"
